620153ba729befb70de99dfad53939d3da819a6e
Jael <jaele.jones@gmail.com>
2016-06-29 20:37:34 -0400

    Task Complete

--- ExpressYourself/Parser.cs
+++ ExpressYourself/Parser.cs
@@ -15,26 +15,57 @@ namespace ExpressYourself
         /// <returns>the title string if it exists</returns>
         public static string GetTitle(string str)
         {
-            // TODO
-            return "";
+            var titleExpression = new Regex(@"Title\: (.*),+");
+            var match = titleExpression.Match(str);
+            if (!match.Success)
+            {
+                return "Title Not Found";
+            }
+            else
+            {
+                return match.Groups[1].Value;
+            }
         }
-
-        public static string GetType(string str)
+    public static string GetType(string str)
         {
-            // TODO
-            return "";
+            var titleExpression = new Regex(@"Type\: (.*),Title");
+            var match = titleExpression.Match(str);
+            if (!match.Success)
+            {
+                return "Type Not Found";
+            }
+            else
+            {
+                return match.Groups[1].Value;
+            }
         }
 
         public static string GetLength(string str)
         {
-            // TODO
-            return "";
+            var lengthExpression = new Regex(@",Length\: (.*)");
+            var match = lengthExpression.Match(str);
+            if (!match.Success)
+            {
+                return "No type found";
+            }
+            else
+            {
+                return match.Groups[1].Value;
+            }
         }
 
         public static bool IsValidLine(string str)
         {
-            // TODO
-            return false;
+            var validLineExpression = new Regex(@"Type: ([a-zA-z]+),Title\: (.*),+Length\: ([0-9a-zA-Z a-z]+)");
+            var match = validLineExpression.Match(str);
+            if (match.Success)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
